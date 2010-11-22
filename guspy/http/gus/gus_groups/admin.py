c9a11604fb18d8b057afb9fa95064b26346d2859
Joran Beasley <Joran Beasley@wormulon.cs.uidaho.edu>
2010-11-21 22:24:44 -0800

    some updates

--- guspy/http/gus/gus_groups/admin.py
+++ guspy/http/gus/gus_groups/admin.py
@@ -1,7 +1,7 @@
 from models import *
 from django.contrib import admin
+from django import forms
 
-admin.site.register(gus_group)
 admin.site.register(gus_user)
 admin.site.register(gus_roles)
 
@@ -9,5 +9,16 @@ admin.site.register(gus_roles)
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
+
+class Roles_Inline(admin.StackedInline):
+	model=gus_roles
+	classes=['collapse']
+	extra=1
+
+class Gus_Group_Admin(admin.ModelAdmin):
+	inlines=[Roles_Inline]
+		
+
+admin.site.register(gus_group,Gus_Group_Admin)
 
 
--- guspy/http/gus/gus_groups/models.py
+++ guspy/http/gus/gus_groups/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User,Group,Permission
 
-# Create your models here.
-
 class gus_user(models.Model):
 	_user=models.OneToOneField(User)
+	def __unicode__(self):
+		return self._user.username
 
 class gus_group(models.Model):
 	_group=models.OneToOneField(Group)
-
+	def __unicode__(self):
+		return self._group.name
+	
 class gus_roles(models.Model):
 	gid=models.ForeignKey(gus_group)
 	uid=models.ManyToManyField(gus_user)
