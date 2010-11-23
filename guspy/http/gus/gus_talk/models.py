6ecaf053532062d6bd77f431b2a84ab6d7a2fdbc
Joran Beasley <Joran Beasley@wormulon.cs.uidaho.edu>
2010-11-22 22:05:15 -0800

    added gus_talk

--- guspy/http/gus/gus_talk/models.py
+++ guspy/http/gus/gus_talk/models.py
@@ -1,24 +1,20 @@
+from gus.gus_groups.models import *
 from django.db import models
-from gus.gus_groups import *
 # Create your models here.
 
-class Board(models.Model):
-	_group=models.OneToOneField(gus_group)
-	_forums=models.OneToManyField(gus_forum)
+class gus_message(models.Model):
+	_text=models.TextField()
+	_author=models.ForeignKey(gus_user,related_name="author")
+	_timestamp=models.DateField(auto_now_add=True)
+	_recipient=models.ForeignKey(gus_user,null=True,blank=True,related_name="recipient")
+
+class gus_thread(models.Model):
+	_title=models.CharField(max_length=100)
+	_messages=models.ManyToManyField(gus_message)
+	_created=models.DateField(auto_now_add=True)
+	_updated=models.DateField(auto_now_add=True)
 
 class gus_forum(models.Model):
 	_title=models.CharField(max_length=100)
-	_topics=models.OneToManyField(gus_topic)
-	_privacy=models.OneToManyField(gus_roles)
-	def add_topic(self,gustopic):
-		print "do something"
-
-class gus_topic(models.Model):
-	_title = models.CharField(max_length=100)
-	_messages=models.OneToManyField(gus_message)
-	def add_message(self,text):
-		print "add a message to this topic"
-
-class gus_message(models.Model):
-	_author=models.ForeignKey(gus_user)
-		
+	_threads=models.ManyToManyField(gus_thread,blank=True)
+	_access=models.ManyToManyField(gus_roles,blank=True)
