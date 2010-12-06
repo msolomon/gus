dea90328443fd95bc8ed2c6a9850db8ac3a13a2f
Chaylo Laurino <laur6383@vandals.uidaho.edu>
2010-12-06 10:25:40 -0800

    adding user control panel mock ups

---
+++ php/http/user_control/events.php
@@ -0,0 +1,12 @@
+<!-- Chaylo Laurino --> 
+
+<form>
+<input type="checkbox" name="event" value="Sack Carthage" /> Sack Carthage
+<br />
+<input type="checkbox" name="event" value="Sack Rome" /> Sack Rome
+<br />
+</form>
+
+<form action="">
+<input type="button" value="Leave Selected">
+</form>
