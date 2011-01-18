3a55deded8af61e119a220be2543051b4186eb80
Joran Beasley <joranbeasley@gmail.com>
2011-01-18 00:54:08 -0800

    added src

---
+++ iteration2/src/src/gus2/gusTestSuite/tests.py
@@ -0,0 +1,17 @@
+"""
+This file demonstrates two different styles of tests (one doctest and one
+unittest). These will both pass when you run "manage.py test".
+
+Replace these with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def setup(self):
+        """
+        setup our test users and groups
+        """
+        return 1
+    
+
---
+++ iteration2/src/src/gus2/gusTestSuite/views.py
@@ -0,0 +1,129 @@
+###
+# Joran Beasle
+# View Controller for Gus Python Team
+# CS383  Fall2010
+#    This collection of functions manages the views that are associated with gus users
+#      It is dependent on gus_groups  models  (gus_user,gus_group,gus_roles)
+#    It is also dependent on gus_groups forms which contains the form definitions for various views
+#    It also utilizes some helper functions (found in file)
+###
+
+#from django.core.exceptions import ObjectDoesNotExist
+#from django.db import IntegrityError 
+from django.shortcuts import render_to_response
+from django.template import  RequestContext #,Context, loader
+from django.http import HttpResponseRedirect ,HttpResponse  
+from django.core.urlresolvers import reverse
+#from django import forms
+#from gus.gus_groups.models import *
+#from gus.gus_groups.forms import *
+#from django.contrib import messages
+from gus2.gus_users.models import gus_user
+from gus2.gus_groups.models import gus_group
+from gus2.gus_roles.models import gus_role
+
+def index(urlRequest):
+    
+    #from django.contrib.auth.forms import UserCreationForm
+    #from gus2.gusTestSuite.forms import SimpleAddUserToGroup
+    #return HttpResponse(SimpleGroupAddForm().as_p())
+    return render_to_response('test/welcome.html',{
+         'users':gus_user.objects.all(),
+         'groups':gus_group.objects.all(),                                          
+         });
+                              
+def addGroup(urlRequest):
+    
+    from gus2.gus_groups.utils import createNewGroup
+    from gus2.gusTestSuite.forms import SimpleGroupAddForm
+    
+    #setup our form
+    if urlRequest.method == 'POST': # If the form has been submitted...
+        form = SimpleGroupAddForm(urlRequest.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            createNewGroup(
+                    form.cleaned_data['group_owner'],
+                    form.cleaned_data['group_name'],
+                    form.cleaned_data['group_description'], 
+                    ""
+                    )
+            
+            return HttpResponseRedirect('/gus_test/') # Redirect after POST
+    else:
+        form = SimpleGroupAddForm() # An unbound form
+
+    
+    return render_to_response('test/form.html',
+                                {
+                                 'submiturl':reverse('gus2.gusTestSuite.views.addGroup'),
+                                 'enctype':'multipart/form-data',
+                                 'form':form,
+                                 'title':'Add New Group',
+                                 'btnlabel':'Create Group',
+                                },
+                                context_instance=RequestContext(urlRequest)
+                              );
+
+def editUser(urlRequest,user_id):
+    user = gus_user.objects.get(pk=user_id)
+    return HttpResponse('Manage User : %s ' % user)
+def editGroup(urlRequest,group_id):
+    from gus2.gus_groups.utils import getGroupRoles
+    group = gus_group.objects.get(pk=group_id)
+    return render_to_response('groups/manageGroup.html',
+        {'group':group,'roles':getGroupRoles(group)}
+        )
+    return HttpResponse('Manage Group : %s ' % group)
+
+def deleteUser(urlRequest,user_id):
+    user = gus_user.objects.get(pk=user_id)
+    return HttpResponse('Delete User : %s ' % user)
+def deleteGroup(urlRequest,group_id):
+    group = gus_group.objects.get(pk=group_id)
+    return HttpResponse('Delete Group : %s ' % group)
+
+def editRole(urlRequest,role_id):
+    from gus2.gusTestSuite.forms import SimpleAddUserToGroup
+    role = gus_role.objects.get(pk=role_id)
+    
+    form_addUser = SimpleAddUserToGroup(role._role_group)
+    return render_to_response('groups/manageRole.html',
+                       {
+                         'role':role,
+                         'formAddUser':form_addUser,
+                       },
+                       context_instance=RequestContext(urlRequest)
+                       )
+
+
+def addUser(urlRequest):
+    from gus2.gusTestSuite.forms import SimpleUserAddForm
+    #setup our form
+    if urlRequest.method == 'POST': # If the form has been submitted...
+        form = SimpleUserAddForm(urlRequest.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            gus_user.objects.create_user(
+                            form.cleaned_data['username'],
+                            form.cleaned_data['email'],
+                            form.cleaned_data['password'],
+                        )
+            return HttpResponseRedirect('/gus_test/') # Redirect after POST
+    else:
+        form = SimpleUserAddForm() # An unbound form
+
+
+    return render_to_response('test/form.html',
+                                {
+                                 'submiturl':reverse('gus2.gusTestSuite.views.addUser'),
+                                 'encType':'multipart/form-data',
+                                 'form':form,
+                                 'title':'Add New User',
+                                 'btnlabel':'Create User',
+                                },
+                                context_instance=RequestContext(urlRequest)
+                              );
+                              
