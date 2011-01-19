6c703577c6e6b57e3e80da76bb74e13bef037480
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-01-18 19:19:21 -0800

    Updated Widget model

--- iteration2/src/src/gus2/gus_widget/models.py
+++ iteration2/src/src/gus2/gus_widget/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Widget(models.Model):
+    """
+    class Widget model is the base model for any gus plug-in
+    
+    Examples of widget "plug-ins"  are calenders, image galleries,
+    forums, and other optional group functionality.  These widgets
+    should have foreign keys to this class.
+    """
+    
+    ### Fields
+    
+    # as i understand, a primary key will be set up automatically
+    # which should suffice for the ID
+    name = models.CharField(max_length=32)
+    
+    # the permissions class doesn't exist, but it should
+    #permissions = models.ForeignKey(permissions)
+    
+    ### Magic Functions
+        
+    def __unicode__(self):
+        """
+        The default string representation of a Widget.
+        
+        @rtype: string
+        @return:    The name of the widget
+        """
+        
+        return self.name
+    
+    
