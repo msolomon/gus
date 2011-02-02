9fced4c96cd534e385a2cec70f10152ef3ce9924
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-02-01 19:22:35 -0800

    Updated installed apps

--- dev/src/src/gus2/settings.py
+++ dev/src/src/gus2/settings.py
@@ -96,12 +96,11 @@ INSTALLED_APPS = (
     'gus2.gus_users',
     'gus2.gus_calendar',
     'gus2.gus_widget',
-    #'gus2.gus_permissions',
     'gus2.gus_forum',
     #'gus2.gus_forum_post',
     #'gus2.gus_forum_thread',
     'gus2.gus_emailer',
-    #'gus2.gus_bill',
+    'gus2.gus_bill',
 
     
     # Uncomment the next line to enable the admin:
