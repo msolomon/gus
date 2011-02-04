63f78bf78071a2ab88688e174172d1e276981cc9
Lee VanGundy <ldv629@live.com>
2011-02-03 17:53:36 -0800

    Start of more tests for roles

--- dev/src/src/gus2/gus_roles/tests.py
+++ dev/src/src/gus2/gus_roles/tests.py
@@ -6,6 +6,7 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from gus2.gus_groups.models import gus_group
 from gus2.gus_roles.models import gus_role
 
 class SimpleTest(TestCase):
@@ -14,6 +15,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.failUnlessEqual(1 + 1, 2)
+
+    def test_create_role(self):
+	"""
+	tests that roles can be created
+	"""
+	print "Test Create Role test_roles"  
+	group_object = gus_group.objects.create_group('test_roles','test','')
+	gus_role.objects.create_role(group_object,'test_role')
+	role = gus_role.objects.get(_role_name='test_role')
+	print role._role_name
 
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
