f6befcfd0560f71be631141978f8130427bbe505
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-02-09 16:41:02 -0800

    Added minor test to Widget class

--- dev/src/src/gus2/gus_widget/tests.py
+++ dev/src/src/gus2/gus_widget/tests.py
@@ -6,7 +6,8 @@ from django.test import TestCase
 from gus2.gus_widget.models import *
 
 class CreateDestroyTest(TestCase):
-    '''Create and destroy a generic widget'''
+    '''Create and destroy a generic widget.
+    Not the most useful test'''
     def test(self):
         self.w = Widget()
         self.assertTrue(self.w is not None)
