7aabfbacd02c72b21d5b21035a656b24d9a50519
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-02-10 18:34:34 -0800

    Removed iteration* directories. To get these back, you must actually use git's version control.

--- iteration2/http/public/gitstats/run.php
+++
@@ -1,5 +0,0 @@
-<?php
-	exec("gitstats/gitstats /home/joran0420/git_gus/gus .");
-	exec("cat index.html|sed 's/Period/Period <a href=\"run.php\">update<\/a>/' > index1.html;mv index1.html index.html");
-	header("location:index.html");	
-?>
