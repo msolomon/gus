2005d3d49dcba60dc444a7aeedbe65ae1c30cc2a
unknown <Kethal@.(none)>
2011-02-13 17:40:21 -0800

    Finished off Tests

--- dev/src/src/gus2/gus_bill/models.py
+++ dev/src/src/gus2/gus_bill/models.py
@@ -10,6 +10,38 @@ class payment(models.Model):
     amtpaid = models.FloatField()
     datepaid = models.DateField(auto_now_add = True)
 
+class BillManager(models.Manager):
+    def create_bill(self, user, group, name, value):
+        """
+        this will create a new user and insert it into the table
+        
+        @type username:string
+        @param username: The Username of the user to add
+        @type email:string
+        @param email: The email of the user to add
+        @type password:string
+        @param password: the plaintext password for the user to add
+        @rtype:gus_roles.models.gus_user
+        @return: the newly created user
+        
+        
+                   
+            >>>gus_user.objects.create_user('username','email','password')
+            (<gus_user object>)
+        """
+        
+        temp = super(BillManager, self).get_query_set().filter(user = user, group = group, name = name)
+        ct = 1
+        Bname1 = name
+        while(len(temp) > 0):
+            Bname1 = name + str(ct)
+            ct += 1
+            temp = super(BillManager, self).get_query_set().filter(user = user, group=group, name=Bname1)
+        
+        return super(BillManager, self).get_query_set().create(user=user, group=group, name=Bname1, value=value)
+                
+
+
 class bill(models.Model):
     """
     Used to keep track of billing for groups.
@@ -22,10 +54,15 @@ class bill(models.Model):
     datecreated = models.DateField(auto_now_add = True)
 
 
+    objects = BillManager()
+
+
 #COMMENT THIS!
     def paid_balance(self):
         temp = payment.objects.filter(mybill = self).aggregate(Sum('amtpaid'))
-        return temp["amtpaid__sum"]
+        if(temp["amtpaid__sum"]):
+            return temp["amtpaid__sum"]
+        return 0
 
     def make_payment(self, Bvalue):
         """
@@ -43,8 +80,11 @@ class bill(models.Model):
         Example:
             >>>modify_bill(test, g1, u1, 12)
         """
+        if(Bvalue + self.paid_balance() > self.value):
+            return self.paid_balance()
         payment.objects.create(mybill = self, amtpaid = Bvalue)
         return self.paid_balance()
-    
-    
-    
+
+    def delete(self):
+        payment.objects.filter(mybill = self).delete()
+        super(bill, self).delete()
--- dev/src/src/gus2/gus_bill/tests.py
+++ dev/src/src/gus2/gus_bill/tests.py
@@ -3,9 +3,20 @@ This file runs unit tests on the gus_bill/models.py file.
 """
 
 from gus2.gus_bill.models import *
+from gus2.gus_users.models import *
+from gus2.gus_groups.models import *
 from django.test import TestCase
 
 class BillTest(TestCase):
+    Guser = None
+    Ggroup = None
+    Bname = "test"
+    Bvalue = 50
+
+    def setUp(self):
+        self.Guser = gus_user.objects.create_user("u1", "email", "pass")
+        self.Ggroup = gus_group.objects.create_group("g1", "email", "pass")
+        
     def test_create_bill(self):
         """
         Tests that the bill was created.
@@ -14,35 +25,48 @@ class BillTest(TestCase):
         The group will be g1
         The initial value will be 11
         """
-        Guser = "u1"
-        Ggroup = "g1"
-        Bname = "test"
-        Bvalue = 11
-#        test = gus_bill.objects.create(user = Guser, group = Ggroup, name = Bname, value = Bvalue)
-#        self.failUnlessEqual(test, 1) # will fail, need better test here
-#        return test
+        test = bill.objects.create_bill(user = self.Guser, group = self.Ggroup, name = self.Bname, value = self.Bvalue)
+        t2 = bill.objects.filter(pk = test.id)
+        self.failUnless(len(t2) == 1 and t2[0].user == self.Guser, "Could not create bill")
+        return test
 
 
-    def test_modify_bill(self):
+    def test_payment(self):
         """
         Tests that the bill from test_create_bill
         Can be modified
         """
-        Nvalue = 5
-#        test = self.test_create_bill()
-#        paid = test.make_payment(Nvalue)
-#        self.failUnlessEqual(paid, 6)
-        
-        
-    def test_get_bill(self):
-        """
-        Tests that get_bill returns the right bill
-        """
-        Guser = "u1"
-        Ggroup = "g1"
-        Bname = "test"
-        Bvalue = 11
-#        test = self.test_create_bill()
-#        test2 = get_bill(Bname, Ggroup, Guser)
-#        self.failUnlessEqual(test, test2)
+        Nvalue = 10
+        test = self.test_create_bill()
+        paid = test.make_payment(Nvalue)
+        self.failUnless(paid == 10, "Unable to make payment")
 
+        paid = test.make_payment(Nvalue)
+        self.failUnless(paid == 20, "Unable to pay more then once.")
+        
+        
+    def test_modify(self):
+        test = self.test_create_bill()
+        test.name = "u2"
+        test.save()
+        t2 = bill.objects.filter(pk = test.id)
+        self.failUnless(len(t2) == 1 and t2[0].name == "u2", "Unable to modify bill")
+
+    def test_delete(self):
+        test = self.test_create_bill()
+        test.delete()
+        t2 = bill.objects.filter(pk = test.id)
+        self.failUnless(len(t2) == 0, "Unable to delete.")
+        
+    def test_many_bill(self):
+        test = self.test_create_bill()
+        Nvalue = 75
+        Nname = "test2"
+        test2 = bill.objects.create_bill(user = self.Guser, group = self.Ggroup, name = Nname, value = Nvalue)
+        t2 = bill.objects.filter(user = self.Guser)
+        self.failUnless(len(t2) == 2, "Unable to create second bill for same user.")
+
+    def test_same_name(self):
+        test = self.test_create_bill()
+        test2 = self.test_create_bill()
+        self.failUnless(test.id > 0 and test2.id > 0 and test.name != test2.name, "Identical bill creation error.")
