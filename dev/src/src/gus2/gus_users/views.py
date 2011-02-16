e2938183863eb78145671aaf3f43b039e0d8980a
Nathan <mazu1444@vandals.uidaho.edu>
2011-02-15 19:07:12 -0800

    Basic view experimentation in users

--- dev/src/src/gus2/gus_users/views.py
+++ dev/src/src/gus2/gus_users/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Something to print')
