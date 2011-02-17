9c691f17ca79aafbf29d3a45f10ea58d839b489e
Mike Solomon <solo4857@vandals.uidaho.edu>
2011-02-16 16:12:03 -0800

    Fixed documentation generation

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -7,3 +7,9 @@ make html
 @echo "Building epydoc documentation in epydoc/"
 epydoc -n gus -o epydoc --html ../gus/
 
+@echo "Running test suite to generate coverage documentation in covhtml/"
+curr=`pwd`
+cd ../gus
+python manage.py test
+cd $curr
+
