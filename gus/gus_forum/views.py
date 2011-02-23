0a06afa9d5244769be8e9a1bafee5b4e7a33a765
unknown <Sverk@.(none)>
2011-02-22 20:34:13 -0800

    Updates to the forum.

--- gus/gus_forum/views.py
+++ gus/gus_forum/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from gus.gus_forum import *
+
+def index(urlRequest):
+	"""
+	"""
+
+	requesting_user = urlRequest.user
+	if requesting_user.is_anonymous():
+		return HttpResponseRedirect('/')
+	#End
+
+	forums = forums.objects.all()
+	return render_to_response('forum/index.html', {"forums":forums}, context_instance=RequestContext{request})
+#End
