7736b393e1ce3526529704e20d9d32866c6e70df
Lee VanGundy <ldv629@live.com>
2011-03-01 19:20:58 -0800

    modified documentation

--- gus/gus_roles/tests.py
+++ gus/gus_roles/tests.py
@@ -100,7 +100,7 @@ class SimpleTest(TestCase):
 	"""
 	@summary: test application of permissions
 	@precondition: L{setUp}
-	tests L{gus_role.with_user_in_group<gus.gus_roles.models.gus_role.permString>}
+	tests L{gus_role.permString<gus.gus_roles.models.gus_role.permString>}
 	"""
 	from django.contrib.auth.models import Permission
 	role = gus_role.objects.create_role(self.testGroup, self.testRole)
@@ -146,4 +146,5 @@ class SimpleTest(TestCase):
         tests L{gus_role.with_group<gus.gus_roles.models.gus_role.with_group>}
         """
         role = gus_role.objects.create_role(self.testGroup, self.testRole)
-	self.failUnlessEqual(role.__unicode__(),'Role :[TestGroup1] testRole',"unexpected unicode response")
+	print role.__unicode__()
+	self.failUnlessEqual(role.__unicode__(),'TestGroup1 (testRole)',"unexpected unicode response")
