b0144c826eb803019af609cfce49e49d30d17a13
sasha <lndndark@gmail.com>
2011-03-03 23:34:21 -0800

    finally fixed the stupid duplicating month bar

--- gus/gus_calendar/urls.py
+++ gus/gus_calendar/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('gus.gus_calendar.views',
     (r"^(\d+)/([A-Za-z]+)/_edit(\d+)/$", "day_edit"),
     (r"^(\d+)/([A-Za-z]+)/(\d+)/$", "day"),
     (r"^(\d+)/([A-Za-z]+)/$", "month"),
-    (r"^", "index"),
+    (r"^", "month"),
     
     
 )
