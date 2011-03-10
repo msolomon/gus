dca246ff806e2c17420d79937799f6bff9fda0be
unknown <Sverk@.(none)>
2011-03-09 17:41:54 -0800

    Some almost working forum updates.

--- gus/gus_forum/urls.py
+++ gus/gus_forum/urls.py
@@ -1,9 +1,9 @@
-"""
-This file matches the URLs for the GUS Gallery
-"""
-
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('gus.gus_forum.views',
-	(r'^', 'index'),
-)
+urlpatterns = patterns('gus.gus_forum.views', 
+	(r"^<group_id>([0-9]+)/forums/$", 'forums'),
+	(r"^<group_id>([0-9]+)/<forum_id>([0-9]+)/threads", 'threads'),
+	(r"^<group_id>([0-9]+)/<forum_id>([0-9]+)/<thread_id>([0-9]+)/posts", 'posts'),
+	(r"^<group_id>([0-9]+)/add_forum", 'add_forum'),
+	(r"^<group_id>([0-9]+)/<forum_id>([0-9]+)/add_thread", 'add_thread'),
+	(r"^<group_id>([0-9]+)/<forum_id>([0-9]+)/<thread_id>([0-9]6+)/add_post", 'add_thread'),)
