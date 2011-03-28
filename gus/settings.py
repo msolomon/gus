d7c4b1dc1556b03fa4b8edd5e27845b5774b6956
Stephen Fischer <stephen@email.com>
2011-03-28 16:42:36 -0700

    Images now display correctly in image galleries.

--- gus/settings.py
+++ gus/settings.py
@@ -48,12 +48,12 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, '../uploads')
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '../media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/uploads/"
+MEDIA_URL = "/include/"
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
