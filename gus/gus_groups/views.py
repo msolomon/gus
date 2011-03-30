513e2c6c367015152f7ae5a6ab6d9bb9d0b075c8
Lee <lee@lee-laptop.(none)>
2011-03-29 18:56:43 -0700

    setting up urls and views for group view

---
+++ gus/gus_groups/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from  django.template import RequestContext
+
+from gus_groups.models import *
+from gus_roles.models import *
+from django import forms
+                                 
+
+
