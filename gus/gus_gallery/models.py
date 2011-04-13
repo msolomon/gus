2a30b294711f97ecb59eca369302ecbcffa7a98c
Stephen Fischer <stephen@email.com>
2011-04-12 19:16:33 -0700

    I'm about to swear in the git commit logs... why you no update, joran's server?

--- gus/gus_gallery/models.py
+++ gus/gus_gallery/models.py
@@ -64,10 +64,10 @@ class gus_image(models.Model):
     A single image belonging to a gus_gallery.
     """
     date_created = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=1000, blank=True, null=True)
+    description = models.CharField(max_length=1000, blank=True, required=False, default='')
     gallery = models.ForeignKey(gus_gallery)
     image = models.ImageField(upload_to='gallery/%Y/%m')
-    name = models.CharField(max_length=500, blank=True, null=True)
+    name = models.CharField(max_length=500, blank=True, required=False, default='')
     user = models.ForeignKey(gus_user)
     
     def __unicode__(self):
