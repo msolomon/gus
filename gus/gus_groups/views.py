a7b925f5b3c4d1fdbadc05eb97646587bf056f57
Lee <ldv629@live.com>
2011-04-12 18:58:09 -0700

    finish copying over

--- gus/gus_groups/views.py
+++ gus/gus_groups/views.py
@@ -121,3 +121,119 @@ def deleteGroup(urlRequest, group_id):
                                'cancel_url' : '/groups/%s/Edit'%group_id},
                               context_instance = RequestContext(urlRequest))
 
+@login_required
+def AddSubgroup(urlRequest,group_id):
+
+    try:
+        group=gus_group.objects.get(pk=group_id)
+        #ensure that we can add groups
+        if not urlRequest.user.has_group_perm(group,'Can add group'):
+            return HttpResponseRedirect('/groups/%s/'%group_id)
+    except:
+        return HttpResponseRedirect('/')
+    if urlRequest.method == 'POST':
+        form = SimpleSubGroupAddForm(urlRequest.POST)
+        if form.is_valid():
+            try:
+                grp = gus_group.objects.create_group(form.cleaned_data['group_name'],form.cleaned_data['group_description'],form.cleaned_data['group_image'] or "")
+                grp.parent_group=group
+                grp.save()
+            except:
+                pass
+            return HttpResponseRedirect('/groups/%s/'%group_id)
+    else:
+        form = SimpleSubGroupAddForm({'parent_group':group_id})
+
+    return render_to_response('test/form.html',
+                                {
+                                 'submiturl':'/groups/%s/Addsubgroup/'%group_id,
+                                 'enctype':'multipart/form-data',
+                                 'form':form,
+                                 'title':'Add Subgroup to %s'%group.group_name,
+                                 'btnlabel':'Create Subgroup',
+                                },
+                                context_instance=RequestContext(urlRequest)
+
+                                )
+@login_required
+def editRole(urlRequest, group_id, user_id):
+    try:
+        group = gus_group.objects.get(pk=group_id)
+        usr = gus_user.objects.get(pk=user_id)
+    except:
+        return HttpResponseRedirect('/groups/%s/'%group_id)
+
+    role = gus_role.objects.with_user_in_group(group, usr)
+
+    try:
+        if urlRequest.POST['newRole']:
+            r2 = gus_role.objects.get(pk=int(urlRequest.POST['newRole']))
+            role.users.remove(usr)
+            r2.users.add(usr)
+            return HttpResponseRedirect("/groups/%s/"%group_id)
+    except:
+        return render_to_response('groups/manageRole.html',
+                       {
+                         'role':role, 'usr':usr, 'group':group
+
+                       },context_instance=RequestContext(urlRequest)
+                       )
+@login_required
+def editRolePerms(urlRequest,role_id):
+    role = gus_role.objects.get(pk=role_id)
+    if urlRequest.method == 'POST':
+        form = RolePermissionForm(urlRequest.POST)
+        if form.is_valid():
+            role._role_permissions.permissions.clear()
+            role._role_permission_level = int(form.cleaned_data['is_superUser'])
+            [role._role_permissions.permissions.add(r) for r in form.cleaned_data['role_permissions']]
+            role.save()
+            g_id = role.group.id
+            return HttpResponseRedirect("/groups/%s/"%g_id)
+    else:
+        if role._role_permission_level == 1:
+            is_superUser = True
+        else:
+            is_superUser = False
+        role_permissions = role._role_permissions.permissions.all()
+        data = {'id':role_id, 'is_superUser':is_superUser, 'role_permissions':role_permissions}
+        form = RolePermissionForm(data)
+
+    #return HttpResponse("WIP")
+
+    return render_to_response('test/form.html',
+                                {
+                                 'submiturl':('/gus_test/Role/EditPerms/%s/'%role_id),
+                                 'encType':'multipart/form-data',
+                                 'form':form,
+                                 'title':'Edit Permissions For Role %s'%role.name,
+                                 'btnlabel':'Save Role',
+                                },
+                                context_instance=RequestContext(urlRequest)
+                              )
+@login_required
+def createRole(urlRequest,group_id):
+    group = gus_group.objects.get(pk=group_id)
+    if urlRequest.method == 'POST':
+        form = RoleCreateForm(urlRequest.POST)
+        if form.is_valid():
+            role = gus_role.objects.create_role(group,form.cleaned_data['role_name'])
+            if form.cleaned_data['is_superUser'] == True:
+                role._role_permission_level = 1
+            [role._role_permissions.permissions.add(r) for r in form.cleaned_data['role_permissions']]
+            role.save()
+            return HttpResponseRedirect("/groups/%s/"%group_id)
+    else:
+        form = RoleCreateForm({'id':group_id})
+        
+#    return HttpResponse("WIP")
+    return render_to_response('test/form.html',
+                                {
+                                 'submiturl':('/gus_test/Role/New/%s/'%group_id),
+                                 'encType':'multipart/form-data',
+                                 'form':form,
+                                 'title':'Create Role for %s'%group.group_name,
+                                 'btnlabel':'Create Role',
+                                },
+                                context_instance=RequestContext(urlRequest)
+                              )
