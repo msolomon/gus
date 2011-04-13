ec0b479282f524575d54fb1476adb5ef2f004c03
Stephen Fischer <stephen@email.com>
2011-04-12 19:05:29 -0700

    Removed name/description as required for images in a gallery

--- gus/gus_gallery/models.py
+++ gus/gus_gallery/models.py
@@ -64,10 +64,10 @@ class gus_image(models.Model):
     A single image belonging to a gus_gallery.
     """
     date_created = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, required=False)
     gallery = models.ForeignKey(gus_gallery)
     image = models.ImageField(upload_to='gallery/%Y/%m')
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=500, required=False)
     user = models.ForeignKey(gus_user)
     
     def __unicode__(self):
